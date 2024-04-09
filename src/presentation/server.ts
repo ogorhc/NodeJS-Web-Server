6487fb877b3529a7b112a06b2178716aae97e7f9
Tu nombre <i.hinojosa@ingreen.es>
2024-04-09 15:35:40 +0200

    Creación de repositorio para deployment en Railway

---
+++ src/presentation/server.ts
@@ -0,0 +1,54 @@
+import express, { Router } from "express";
+import path from "path";
+
+interface ServerOptions {
+  port: number;
+  public_path: string;
+  routes: Router;
+}
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+  private readonly routes: Router;
+
+  constructor(options: ServerOptions) {
+    const { port, public_path = "public", routes } = options;
+    this.port = port;
+    this.publicPath = public_path;
+    this.routes = routes;
+  }
+  async start() {
+    /*
+		Middlewares
+	*/
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    /*
+		Public Folder
+	*/
+    this.app.use(express.static(this.publicPath));
+
+    /* 
+	Routes
+	*/
+
+    this.app.use(this.routes);
+
+    /* SPA */
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(
+        __dirname + `../../../${this.publicPath}/index.html`
+      );
+      res.sendFile(indexPath);
+      return;
+    });
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on PORT: ${this.port}`);
+    });
+  }
+}
